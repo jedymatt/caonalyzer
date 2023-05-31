4beb2091ad8db7af14753e1763ad77acc40c78cb
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-01 01:16:02 +0800

    Update globals.dart

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,3 +1,4 @@
+import 'package:caonalyzer/object_detectors/enums/preferred_mode.dart';
 import 'package:get/get.dart';
 import 'package:camera/camera.dart';
 
@@ -5,3 +6,4 @@ const String kAppName = 'Cao-nalyzer';
 
 List<CameraDescription> cameras = Get.put([], permanent: true);
 String host = Get.put('192.168.1.4', permanent: true);
+PreferredMode preferredMode = Get.put(PreferredMode.online, permanent: true);
