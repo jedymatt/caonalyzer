fae59a896e252a9986af3c425b023fb4c4bfc35c
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-05-31 14:11:49 +0800

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:caonalyzer/globals.dart';
 import 'package:caonalyzer/detector.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:image/image.dart' as img;
