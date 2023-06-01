cc371d078d5047b38e73b230ae304355bfbbfa5b
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-01 16:22:21 +0800

    Update object_detector.dart

--- lib/object_detectors/object_detector.dart
+++ lib/object_detectors/object_detector.dart
@@ -1,10 +1,19 @@
 import 'package:image/image.dart' as img;
 import 'package:tflite_flutter_helper_plus/tflite_flutter_helper_plus.dart';
 
-import 'package:caonalyzer/object_detectors/models/object_detection_output.dart';
+import './object_detection_output.dart';
+import './ops/scale_op.dart';
 
 abstract class ObjectDetector {
-  TensorImage preProcessImage(img.Image image);
+  TensorImage preProcessImage(img.Image image) {
+    ImageProcessor imageProcessor = ImageProcessorBuilder()
+        .add(ScaleOp(640, ResizeMethod.bilinear))
+        .build();
 
-  Future<List<ObjectDetectionOutput>> runInference(TensorImage tensorImage);
+    TensorImage tensorImage = TensorImage.fromImage(image);
+
+    return imageProcessor.process(tensorImage);
+  }
+
+  Future<ObjectDetectionOutput> runInference(TensorImage tensorImage);
 }
