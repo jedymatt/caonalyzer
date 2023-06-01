d3fc9d5e44ec824038ea6e42eb915b7a10193b74
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-01 17:13:47 +0800

    Update image_screen.dart

--- lib/ui/screens/image_screen.dart
+++ lib/ui/screens/image_screen.dart
@@ -1,33 +1,46 @@
 import 'dart:typed_data';
 
+import 'package:caonalyzer/globals.dart';
 import 'package:caonalyzer/object_detectors/box_converter.dart';
 import 'package:caonalyzer/object_detectors/object_detection_output.dart';
 import 'package:flutter/material.dart';
-import 'package:image/image.dart' as img;
+import 'package:image/image.dart' as imgLib;
 
 class ImageScreen extends StatelessWidget {
-  const ImageScreen(this.image, this.objectDetectionOutput, {super.key});
+  const ImageScreen(this.image, this.output, {super.key});
 
-  final img.Image image;
-  final ObjectDetectionOutput objectDetectionOutput;
+  final imgLib.Image image;
+  final ObjectDetectionOutput output;
 
   @override
   Widget build(BuildContext context) {
     // draw bounding boxes on image
     final boxes = BoxConverter.convert(
-      objectDetectionOutput.detectionBoxes,
+      output.detectionBoxes,
       height: image.height,
       width: image.width,
     );
 
     for (Rect box in boxes) {
-      img.drawRect(
+      imgLib.drawRect(
         image,
         box.left.toInt(),
         box.top.toInt(),
         box.right.toInt(),
         box.bottom.toInt(),
-        img.getColor(0, 255, 0),
+        imgLib.getColor(0, 255, 0),
+      );
+
+      final label = labels[output.detectionClasses[boxes.indexOf(box)]];
+      final score = output.detectionScores[boxes.indexOf(box)];
+
+      imgLib.drawString(
+        image,
+        imgLib.arial_14,
+        box.left.toInt(),
+        box.top.toInt(),
+        '$label ${(score * 100).toStringAsFixed(2)}%',
+        color: imgLib.getColor(0, 255, 0),
       );
     }
 
@@ -37,11 +50,11 @@ class ImageScreen extends StatelessWidget {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Image.memory(img.encodePng(image) as Uint8List),
+              Image.memory(imgLib.encodePng(image) as Uint8List),
               const SizedBox.square(
                 dimension: 50,
               ),
-              Text(objectDetectionOutput.numDetections.toString()),
+              Text('${output.numDetections}'),
             ],
           ),
         ),
