33e15c2cd4d189592ecf7c11e186a699d55b8fbe
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-03 17:43:05 +0800

    Create object_detection_response.dart

---
+++ lib/services/tf_serving/object_detection_response.dart
@@ -0,0 +1,52 @@
+import 'package:caonalyzer/globals.dart';
+import 'package:caonalyzer/object_detectors/types/bounding_box.dart';
+import 'package:caonalyzer/object_detectors/types/object_detection_output.dart';
+
+class ObjectDetectionResponse {
+  final int numDetections;
+  final List<List<double>> detectionBoxes;
+  final List<int> detectionClasses;
+  final List<double> detectionScores;
+
+  ObjectDetectionResponse({
+    required this.numDetections,
+    required this.detectionBoxes,
+    required this.detectionClasses,
+    required this.detectionScores,
+  });
+
+  factory ObjectDetectionResponse.fromMap(Map<String, dynamic> map) {
+    final numDetections = (map['num_detections'][0] as double).toInt();
+
+    return ObjectDetectionResponse(
+      numDetections: numDetections,
+      detectionBoxes: (map['detection_boxes'][0] as List)
+          .map<List<double>>((e) => e.cast<double>())
+          .toList()
+          .sublist(0, numDetections),
+      detectionClasses: (map['detection_classes'][0] as List)
+          .map<int>((e) => e.toInt())
+          .toList()
+          .sublist(0, numDetections),
+      detectionScores: (map['detection_scores'][0] as List)
+          .cast<double>()
+          .sublist(0, numDetections),
+    );
+  }
+
+  List<ObjectDetectionOutput> toObjectDetectionOutputs() {
+    return List.generate(
+      numDetections,
+      (index) => ObjectDetectionOutput(
+        labels[detectionClasses[index] - 1],
+        detectionScores[index],
+        BoundingBox(
+          left: detectionBoxes[index][1],
+          top: detectionBoxes[index][0],
+          right: detectionBoxes[index][3],
+          bottom: detectionBoxes[index][2],
+        ),
+      ),
+    );
+  }
+}
