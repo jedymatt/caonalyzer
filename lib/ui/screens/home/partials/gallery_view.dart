db9f47af37da2fe29ae3f8c23be4cfe06b367505
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-03 14:43:01 +0800

    Update gallery_view.dart

---
+++ lib/ui/screens/home/partials/gallery_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GalleryView extends StatelessWidget {
+  const GalleryView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [Placeholder()],
+      ),
+    );
+  }
+}
