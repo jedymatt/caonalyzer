d7a8c8ba28ebf589df15a9f0f315224dc0c24425
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-03 14:47:09 +0800

    Update camera_screen.dart

--- lib/ui/screens/camera_screen.dart
+++ lib/ui/screens/camera_screen.dart
@@ -1,8 +1,10 @@
 import 'package:camera/camera.dart';
 import 'package:caonalyzer/globals.dart';
+import 'package:caonalyzer/object_detectors/box_converter.dart';
+import 'package:caonalyzer/object_detectors/object_detectors.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:image/image.dart' as img;
+import 'package:image/image.dart' as imageLib;
 
 class CameraScreen extends StatefulWidget {
   const CameraScreen({super.key});
@@ -14,15 +16,23 @@ class CameraScreen extends StatefulWidget {
 class _CameraScreenState extends State<CameraScreen>
     with WidgetsBindingObserver, TickerProviderStateMixin {
   late CameraController cameraController;
+  late ObjectDetector objectDetector;
+  imageLib.Image? currentImage;
 
   @override
   void initState() {
     super.initState();
 
-    _initializeCameraController(cameras[0]);
+    objectDetector = preferredMode.value.objectDetector;
 
-    // ignore: avoid_print
-    print(cameras);
+    _initializeCameraController(cameras[0]);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+
+    cameraController.dispose();
   }
 
   @override
@@ -56,14 +66,13 @@ class _CameraScreenState extends State<CameraScreen>
   void _initializeCameraController(CameraDescription description) async {
     cameraController = CameraController(
       description,
-      ResolutionPreset.max,
+      ResolutionPreset.low,
       enableAudio: false,
     );
 
     cameraController.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
+      if (!mounted) return;
+
       setState(() {
         cameraController.startImageStream(_imageStream);
       });
@@ -111,14 +120,64 @@ class _CameraScreenState extends State<CameraScreen>
     );
   }
 
-  _imageStream(CameraImage image) async {
+  void _imageStream(CameraImage image) async {
     final WriteBuffer allBytes = WriteBuffer();
+
     for (final Plane plane in image.planes) {
       allBytes.putUint8List(plane.bytes);
     }
+
     final bytes = allBytes.done().buffer.asUint8List();
 
-    // preferredMode.value.objectDetector.runInference();
+    final Size imageSize =
+        Size(image.width.toDouble(), image.height.toDouble());
+
+    final imageLib.Image imageFromBytes = imageLib.Image.fromBytes(
+      imageSize.width.toInt(),
+      imageSize.height.toInt(),
+      bytes,
+      format: imageLib.Format.rgb,
+    );
+
+    final tensorImage = objectDetector.preProcessImage(imageFromBytes);
+
+    final output = await objectDetector.runInference(tensorImage);
+
+    final postProccessedImage = tensorImage.image.clone();
+
+    // draw bounding boxes on image
+    final boxes = BoxConverter.convert(
+      output.detectionBoxes,
+      height: image.height,
+      width: image.width,
+    );
+
+    for (Rect box in boxes) {
+      imageLib.drawRect(
+        postProccessedImage,
+        box.left.toInt(),
+        box.top.toInt(),
+        box.right.toInt(),
+        box.bottom.toInt(),
+        imageLib.getColor(0, 255, 0),
+      );
+
+      final label = labels[output.detectionClasses[boxes.indexOf(box)]];
+      final score = output.detectionScores[boxes.indexOf(box)];
+
+      imageLib.drawStringWrap(
+        postProccessedImage,
+        imageLib.arial_14,
+        box.left.toInt(),
+        box.top.toInt(),
+        '$label ${(score * 100).toStringAsFixed(2)}%',
+        color: imageLib.getColor(0, 255, 0),
+      );
+    }
+
+    setState(() {
+      currentImage = postProccessedImage;
+    });
   }
 }
 
