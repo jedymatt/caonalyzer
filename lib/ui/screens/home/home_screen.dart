e9c0bee3cdd94215e66fcbbbcccc20dc5c17dad6
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-03 14:47:05 +0800

    Update home_screen.dart

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,13 +1,9 @@
 import 'package:caonalyzer/globals.dart';
-import 'package:caonalyzer/object_detectors/object_detection_output.dart';
-import 'package:caonalyzer/object_detectors/object_detector.dart';
 import 'package:caonalyzer/ui/screens/camera_screen.dart';
-import 'package:caonalyzer/ui/screens/image_screen.dart';
+import 'package:caonalyzer/ui/screens/home/partials/gallery_view.dart';
+import 'package:caonalyzer/ui/screens/home/partials/main_view.dart';
 import 'package:caonalyzer/ui/screens/settings_screen.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:image/image.dart' as img;
-import 'package:image_picker/image_picker.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,16 +13,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late ObjectDetector objectDetector;
   String message = '';
   bool isBusy = false;
-  ObjectDetectionOutput objectDetectionOutput = ObjectDetectionOutput.empty();
+  int viewIndex = 0;
+  List<Widget> views = [
+    const MainView(),
+    const GalleryView(),
+  ];
 
   @override
   void initState() {
     super.initState();
-
-    objectDetector = preferredMode.value.objectDetector;
   }
 
   @override
@@ -46,8 +43,13 @@ class _HomeScreenState extends State<HomeScreen> {
               icon: const Icon(Icons.settings),
             )
           ],
-          bottom: const TabBar(
-            tabs: <Widget>[
+          bottom: TabBar(
+            onTap: (value) {
+              setState(() {
+                viewIndex = value;
+              });
+            },
+            tabs: const <Widget>[
               Tab(
                 icon: Icon(Icons.home),
                 iconMargin: EdgeInsets.zero,
@@ -61,21 +63,7 @@ class _HomeScreenState extends State<HomeScreen> {
             ],
           ),
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              ElevatedButton(
-                onPressed: pickImage,
-                child: const Text('Pick Image'),
-              ),
-              isBusy
-                  ? const CircularProgressIndicator()
-                  : const SizedBox.shrink(),
-              Text('Outputs count: ${objectDetectionOutput.numDetections}'),
-            ],
-          ),
-        ),
+        body: buildView(),
         floatingActionButton: FloatingActionButton.extended(
           onPressed: navigateToCamera,
           label: const Text('Camera'),
@@ -85,54 +73,13 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  void navigateToCamera() {
-    Navigator.of(context)
-        .push(MaterialPageRoute(builder: (context) => const CameraScreen()));
+  Widget buildView() {
+    return views[viewIndex];
   }
 
-  void pickImage() async {
-    final imagePicker = ImagePicker();
-    XFile? fileImage = await imagePicker.pickImage(source: ImageSource.gallery);
-
-    if (fileImage == null) return;
-
-    final imageBytes = await fileImage.readAsBytes();
-    final img.Image decodedImage = img.decodeImage(imageBytes)!;
-
-    final tensorImage = objectDetector.preProcessImage(decodedImage);
-
-    try {
-      setState(() {
-        isBusy = true;
-      });
-      objectDetectionOutput = await objectDetector.runInference(tensorImage);
-      setState(() {
-        isBusy = false;
-      });
-    } on Exception catch (e) {
-      setState(() {
-        isBusy = false;
-      });
-
-      if (!mounted) return;
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text(e.toString()),
-        ),
-      );
-
-      if (!kDebugMode) {
-        return;
-      }
-    }
-    if (!mounted) return;
-
+  void navigateToCamera() {
     Navigator.of(context).push(MaterialPageRoute(
-      builder: (context) => ImageScreen(
-        tensorImage.image.clone(),
-        objectDetectionOutput,
-      ),
+      builder: (context) => const CameraScreen(),
     ));
   }
 }
