b831ec0c9d45f002f31683e06a22fd672ae0cc16
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-04 01:07:43 +0800

    Update object_detector.dart

--- lib/object_detectors/object_detector.dart
+++ lib/object_detectors/object_detector.dart
@@ -1,16 +1,8 @@
-import 'dart:typed_data';
-
-import 'package:image/image.dart' show Image;
+import 'package:image/image.dart';
 
 import 'object_detection_output.dart';
 
 abstract class ObjectDetector {
   Image preprocessImage(Image image);
   Future<List<ObjectDetectionOutput>> runInference(Image image);
-  Future<List<ObjectDetectionOutput>> runInferenceOnFrame(
-    List<Uint8List> bytes,
-    int imageHeight,
-    int imageWidth,
-  );
-  void dispose();
 }
