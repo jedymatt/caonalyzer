70e21832ad6f8e1624c6fc783823892e06ca646c
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-04 01:08:17 +0800

    Create realtime_pytorch_object_detector.dart

---
+++ lib/services/realtime_pytorch_object_detector.dart
@@ -0,0 +1,39 @@
+import 'dart:typed_data';
+
+import 'package:caonalyzer/object_detectors/object_detectors.dart';
+import 'package:caonalyzer/services/pytorch_object_detector.dart';
+
+class RealtimePytorchObjectDetector {
+  final PytorchObjectDetector objectDetector = PytorchObjectDetector();
+
+  Future<List<ObjectDetectionOutput>> runInferenceOnFrame(
+    List<Uint8List> bytes,
+    int imageHeight,
+    int imageWidth,
+  ) async {
+    final model = await objectDetector.getModel();
+
+    final results = await model.yoloOnFrame(
+      bytesList: bytes,
+      imageHeight: imageHeight,
+      imageWidth: imageWidth,
+    );
+
+    return results.map((e) {
+      final label = e['tag'];
+      final confidence = e['box'][4]; // this is api bug
+      final rect = BoundingBox(
+        left: e['box'][0],
+        top: e['box'][1],
+        right: e['box'][2],
+        bottom: e['box'][3],
+      );
+
+      return ObjectDetectionOutput(
+        label,
+        confidence,
+        rect,
+      );
+    }).toList();
+  }
+}
