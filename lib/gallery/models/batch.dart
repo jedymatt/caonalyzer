9c1542ae3d03093e505fc095bec0320b89a1ee49
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-08 18:02:33 +0800

    Update batch.dart

--- lib/gallery/models/batch.dart
+++ lib/gallery/models/batch.dart
@@ -1,15 +1,17 @@
 import 'picture.dart';
 
 class Batch {
-  final String title;
-  final String path;
-  final String date;
-  final List<Picture> pictures;
-
   Batch({
     required this.title,
     required this.path,
     required this.date,
     required this.pictures,
   });
+
+  final String title;
+  final String path;
+  final String date;
+  final List<Picture> pictures;
+
+  String? get thumbnail => pictures.elementAtOrNull(0)?.path;
 }
