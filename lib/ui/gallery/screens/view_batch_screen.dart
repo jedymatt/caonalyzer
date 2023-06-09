378e22a364b509f33cb79d4f8fd1887984fe5e7d
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-09 18:32:48 +0800

    Add overlay hight and check to selected images

--- lib/ui/gallery/screens/view_batch_screen.dart
+++ lib/ui/gallery/screens/view_batch_screen.dart
@@ -1,10 +1,7 @@
 import 'package:caonalyzer/gallery/models/batch.dart';
 import 'package:caonalyzer/gallery/models/picture.dart';
-import 'package:caonalyzer/globals.dart';
 import 'package:caonalyzer/ui/gallery/screens/image_screen.dart';
-import 'package:caonalyzer/ui/screens/camera_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 class ViewBatchScreen extends StatefulWidget {
   const ViewBatchScreen(this.batch, {super.key});
@@ -22,42 +19,80 @@ class _ViewBatchScreenState extends State<ViewBatchScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: _buildAppBar(),
-        body: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-          ),
-          itemCount: widget.batch.pictures.length,
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(4),
-              child: InkWell(
-                onLongPress: _isSelecting
-                    ? null
-                    : () {
-                        setState(() {
-                          _isSelecting = true;
-                        });
-                        addToSelection(widget.batch.pictures[index]);
-                      },
-                onTap: _isSelecting
-                    ? () => toggleSelection(widget.batch.pictures[index])
-                    : () => redirectToImageViewer(widget.batch.pictures[index]),
-                child: GridTile(
-                  footer: Center(
-                      child: Text(widget.batch.pictures[index].id.toString())),
-                  child: Image.network(
-                    widget.batch.pictures[index].path,
-                    fit: BoxFit.cover,
-                    errorBuilder: (context, error, stackTrace) =>
-                        const Icon(Icons.error),
+      appBar: _buildAppBar(),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+        ),
+        itemCount: widget.batch.pictures.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.all(4),
+            child: Column(
+              children: [
+                Expanded(
+                  child: InkWell(
+                    onLongPress: () {
+                      if (_isSelecting) return;
+
+                      setState(() {
+                        _isSelecting = true;
+                      });
+                      addToSelection(widget.batch.pictures[index]);
+                    },
+                    onTap: _isSelecting
+                        ? () =>
+                            toggleSelection(widget.batch.pictures[index])
+                        : () => redirectToImageViewer(
+                            widget.batch.pictures[index]),
+                    child: Stack(
+                      fit: StackFit.expand,
+                      children: [
+                        Image.network(
+                          widget.batch.pictures[index].path,
+                          fit: BoxFit.cover,
+                          errorBuilder: (context, error, stackTrace) =>
+                              const Icon(Icons.error),
+                        ),
+                        // checkmark in top right corner
+                        if (_isSelecting)
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: Padding(
+                              padding: const EdgeInsets.all(4),
+                              child: Icon(
+                                _selectedPictures.contains(
+                                        widget.batch.pictures[index])
+                                    ? Icons.check_circle
+                                    : Icons.radio_button_unchecked,
+                                color: Colors.blue,
+                              ),
+                            ),
+                          ),
+                        // highlight box around the image
+                        if (_isSelecting &&
+                            _selectedPictures
+                                .contains(widget.batch.pictures[index]))
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.blue,
+                                width: 2,
+                              ),
+                            ),
+                          ),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            );
-          },
-        ),
-        bottomNavigationBar: _buildBottomNavigationBar(context));
+                Text(widget.batch.pictures[index].id.toString()),
+              ],
+            ),
+          );
+        },
+      ),
+      bottomNavigationBar: _buildBottomNavigationBar(),
+    );
   }
 
   void redirectToImageViewer(Picture picture) {
@@ -69,58 +104,56 @@ class _ViewBatchScreenState extends State<ViewBatchScreen> {
     ));
   }
 
-  BottomNavigationBar? _buildBottomNavigationBar(BuildContext context) {
-    if (_isSelecting && _selectedPictures.isEmpty) return null;
+  Widget? _buildBottomNavigationBar() {
+    return _isSelecting
+        ? _selectingImageBottomNav()
+        : _notSelectingImageBottomNav();
+  }
 
-    if (_isSelecting) {
-      return BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.delete),
-            label: 'Delete',
-          ),
-          // detect moldy cacao beans
-          BottomNavigationBarItem(
-            icon: Icon(Icons.document_scanner),
-            label: 'Scan',
-          ),
-        ],
-        onTap: (index) {
-          switch (index) {
-            case 0:
-              // todo: delete selected pictures
-              break;
-            case 1:
-              // todo: detect moldy cacao beans
-              break;
-          }
-        },
-      );
-    }
+  Widget? _selectingImageBottomNav() {
+    if (_selectedPictures.isEmpty) return null;
 
     return BottomNavigationBar(
       items: const [
         BottomNavigationBarItem(
-          activeIcon: Icon(Icons.add_circle),
-          icon: Icon(Icons.add),
-          label: 'Add',
+          icon: Icon(Icons.delete),
+          label: 'Delete',
         ),
-        // detect moldy cacao beans
         BottomNavigationBarItem(
           icon: Icon(Icons.document_scanner),
           label: 'Scan',
         ),
       ],
+    );
+  }
+
+  Widget _notSelectingImageBottomNav() {
+    return BottomNavigationBar(
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.add_a_photo),
+          label: 'Camera',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.document_scanner),
+          label: 'Scan',
+        ),
+        // more
+        BottomNavigationBarItem(
+          icon: Icon(Icons.more_vert),
+          label: 'More',
+        ),
+      ],
       onTap: (index) {
         switch (index) {
           case 0:
-            // add image
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => const CameraScreen(),
-            ));
+            // camera
             break;
           case 1:
-            // detect moldy cacao beans
+            // scan
+            break;
+          case 2:
+            // open more menu
             break;
         }
       },
@@ -148,11 +181,38 @@ class _ViewBatchScreenState extends State<ViewBatchScreen> {
   }
 
   AppBar _buildAppBar() {
-    List<Widget> actions = [];
-    Widget title = Text(widget.batch.title);
+    return _isSelecting ? _appBarSelection() : _appBarDefault();
+  }
 
-    if (_isSelecting) {
-      actions = [
+  AppBar _appBarDefault() {
+    return AppBar(
+      title: Text(widget.batch.title),
+      actions: [
+        IconButton(
+          onPressed: () {
+            setState(() {
+              _isSelecting = true;
+            });
+          },
+          icon: const Icon(Icons.checklist),
+        ),
+      ],
+    );
+  }
+
+  AppBar _appBarSelection() {
+    return AppBar(
+      title: Text('${_selectedPictures.length} selected'),
+      leading: IconButton(
+        onPressed: () {
+          setState(() {
+            _isSelecting = false;
+            _selectedPictures.clear();
+          });
+        },
+        icon: const Icon(Icons.arrow_back),
+      ),
+      actions: [
         if (_selectedPictures.length != widget.batch.pictures.length)
           IconButton(
             onPressed: () {
@@ -172,24 +232,7 @@ class _ViewBatchScreenState extends State<ViewBatchScreen> {
             },
             icon: const Icon(Icons.deselect),
           ),
-      ];
-      title = Text('${_selectedPictures.length} selected');
-    }
-
-    return AppBar(
-      leading: _isSelecting
-          ? IconButton(
-              onPressed: () {
-                setState(() {
-                  _isSelecting = false;
-                  _selectedPictures.clear();
-                });
-              },
-              icon: const Icon(Icons.arrow_back),
-            )
-          : null,
-      title: title,
-      actions: actions,
+      ],
     );
   }
 }
