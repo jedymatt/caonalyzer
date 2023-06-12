a32f5ba4b502e421fe362df3a90775f48f61fe51
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-12 16:05:53 +0800

    Append images to batch directory

--- lib/ui/screens/camera/batch_confirmation_screen.dart
+++ lib/ui/screens/camera/batch_confirmation_screen.dart
@@ -3,17 +3,16 @@ import 'dart:io';
 import 'package:caonalyzer/gallery/gallery_writer.dart';
 import 'package:caonalyzer/gallery/models/batch.dart';
 import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
 
 import '../../gallery/screens/view_batch_screen.dart';
-
-enum BottomNavigationIndex {
-  retake,
-  confirm,
-}
+import '../camera_screen.dart';
 
 class BatchConfirmationScreen extends StatefulWidget {
-  const BatchConfirmationScreen(this.images, {super.key});
+  const BatchConfirmationScreen(this.batchPath, this.images, {super.key});
 
+  final String batchPath;
   final List<String> images;
 
   @override
@@ -22,33 +21,58 @@ class BatchConfirmationScreen extends StatefulWidget {
 }
 
 class _BatchConfirmationScreenState extends State<BatchConfirmationScreen> {
-  late final String batchPath;
+  late final PageController _pageController;
 
   @override
   void initState() {
     super.initState();
-
-    batchPath = GalleryWriter.instance.generateBatchPath();
+    _pageController = PageController();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Image.file(File(widget.images.first)),
+      appBar: AppBar(
+        title: Text(widget.batchPath),
+      ),
+      body: PhotoViewGallery(
+        pageOptions: widget.images
+            .map(
+              (image) => PhotoViewGalleryPageOptions(
+                imageProvider: FileImage(File(image)),
+                heroAttributes: PhotoViewHeroAttributes(tag: image),
+                minScale: PhotoViewComputedScale.contained,
+                maxScale: PhotoViewComputedScale.covered * 2.0,
+              ),
+            )
+            .toList(),
+        backgroundDecoration: BoxDecoration(
+          color: Theme.of(context).canvasColor,
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
         onTap: (index) {
-          if (BottomNavigationIndex.values[index] ==
-              BottomNavigationIndex.retake) {
+          if (index == 0) {
+            // retake
+          }
+
+          if (index == 1) {
             Navigator.of(context).pop();
-          } else {
+          }
+
+          if (index == 2) {
             confirm();
           }
         },
+        currentIndex: 1,
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.camera_alt),
             label: 'Retake',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_a_photo),
+            label: 'Add',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.check),
@@ -60,16 +84,12 @@ class _BatchConfirmationScreenState extends State<BatchConfirmationScreen> {
     );
   }
 
-  void confirm() {
-    // final batchPath =
-    //     widget.batchPath ?? GalleryWriter.instance.generateBatchPath();
-    //
-    // if (widget.batchPath == null) {
-    //   GalleryWriter.instance.createDirectory(batchPath);
-    // }
-    //
-    // String path =
-    //     await GalleryWriter.instance.appendImage(file.path, batchPath);
-    // redirect to view batch screen
+  void confirm() async {
+    GalleryWriter.instance.createDirectory(widget.batchPath);
+
+    await GalleryWriter.instance.appendImages(widget.images, widget.batchPath);
+    // todo: redirect to view batch screen
+    if (!mounted) return;
+    Navigator.popUntil(context, (route) => route.isFirst);
   }
 }
