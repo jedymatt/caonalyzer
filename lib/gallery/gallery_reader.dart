0f29918d4300e40aa7e8a98c2ac58a58e94d92e7
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-13 12:34:02 +0800

    Update gallery_reader.dart

--- lib/gallery/gallery_reader.dart
+++ lib/gallery/gallery_reader.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
+import 'package:caonalyzer/gallery/models/batch.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
 
 class GalleryReader {
   Future<List<File>> getImages(String path) async {
@@ -9,5 +11,22 @@ class GalleryReader {
     final dir = Directory('${externalStorageDir.path}/$path');
 
     return dir.listSync().map((e) => File(e.path)).toList();
+  }
+
+  static Future<List<Batch>> getBatches() async {
+    final Directory externalStorageDir = (await getExternalStorageDirectory())!;
+
+    final dir = Directory(externalStorageDir.path);
+
+    return dir
+        .listSync()
+        .map((e) =>
+        Batch(
+          title: path.basename(e.path),
+          dirPath: e.path,
+          images: Directory(e.path).listSync().map((e) => e.path).toList(),
+        ))
+        .where((element) => element.images.isNotEmpty)
+        .toList();
   }
 }
