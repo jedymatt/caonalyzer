bee71153d0714dabc3fb3f0afb301b0a7127ab40
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-13 14:42:57 +0800

    Update batch.dart

--- lib/gallery/models/batch.dart
+++ lib/gallery/models/batch.dart
@@ -1,3 +1,4 @@
+import 'package:path/path.dart' as path_lib;
 class Batch {
   Batch({
     required this.title,
@@ -10,4 +11,11 @@ class Batch {
   final List<String> images;
 
   String get thumbnail => images.first;
+
+  String get relativeDirPath => path_lib.basename(dirPath);
+
+  @override
+  String toString() {
+    return 'Batch(title: $title, dirPath: $dirPath, images: $images)';
+  }
 }
