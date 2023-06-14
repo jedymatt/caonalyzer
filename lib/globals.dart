b8e82bba1d01e7319005cd200f70eca2fd6740cf
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-14 14:49:50 +0800

    Update globals.dart

--- lib/globals.dart
+++ lib/globals.dart
@@ -4,6 +4,8 @@ import 'package:camera/camera.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
+import 'gallery/models/batch.dart';
+
 const String kAppName = 'Cao-nalyzer';
 
 final box = GetStorage();
@@ -12,6 +14,7 @@ RxList<CameraDescription> cameras = <CameraDescription>[].obs;
 RxString host = '192.168.1.8'.obs;
 Rx<PreferredMode> preferredMode = PreferredMode.offline.obs;
 RxList<String> labels = <String>[].obs;
+final batches = <Batch>[].obs;
 
 Future<void> initGlobals() async {
   cameras.value = await availableCameras();
