d0b0d7f316fdb6a9ca50d4285faff8c7e68d692d
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-14 19:54:21 +0800

    Update metadata_writer.dart

--- lib/gallery/metadata_writer.dart
+++ lib/gallery/metadata_writer.dart
@@ -3,14 +3,14 @@ import 'dart:io';
 import 'package:caonalyzer/gallery/models/image_metadata.dart';
 
 class MetadataWriter {
-  void create(String imagePath, ImageMetadata metadata) {
+  static void create(String imagePath, ImageMetadata metadata) {
     // create metadata file in imagePath like this:
     // /path/to/image.jpg -> /path/to/image.jpg.metadata.json
     final metadataFile = File('$imagePath.metadata.json');
     metadataFile.writeAsStringSync(metadata.toJson());
   }
 
-  void delete(String imagePath) {
+  static void delete(String imagePath) {
     // delete metadata file in imagePath like this:
     // /path/to/image.jpg -> /path/to/image.jpg.metadata.json
     final metadataFile = File('$imagePath.metadata.json');
