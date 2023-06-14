438f2f83e83f43bd62a58dafcaec4d7308c80371
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-14 19:16:02 +0800

    Update metadata_writer.dart

--- lib/gallery/metadata_writer.dart
+++ lib/gallery/metadata_writer.dart
@@ -1,3 +1,19 @@
-class MetadataWriter {
+import 'dart:io';
 
+import 'package:caonalyzer/gallery/models/image_metadata.dart';
+
+class MetadataWriter {
+  void create(String imagePath, ImageMetadata metadata) {
+    // create metadata file in imagePath like this:
+    // /path/to/image.jpg -> /path/to/image.jpg.metadata.json
+    final metadataFile = File('$imagePath.metadata.json');
+    metadataFile.writeAsStringSync(metadata.toJson());
+  }
+
+  void delete(String imagePath) {
+    // delete metadata file in imagePath like this:
+    // /path/to/image.jpg -> /path/to/image.jpg.metadata.json
+    final metadataFile = File('$imagePath.metadata.json');
+    metadataFile.deleteSync();
+  }
 }
