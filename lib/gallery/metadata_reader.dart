177b7c7029c3a56fcc9a5c082015931e5b4d0112
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-14 19:15:59 +0800

    Update metadata_reader.dart

--- lib/gallery/metadata_reader.dart
+++ lib/gallery/metadata_reader.dart
@@ -1,3 +1,14 @@
-class MetadataReader {
+import 'dart:io';
 
+import 'package:caonalyzer/gallery/models/image_metadata.dart';
+
+class MetadataReader {
+  ImageMetadata read(String imagePath) {
+    // read metadata file in imagePath like this:
+    // /path/to/image.jpg -> /path/to/image.jpg.metadata.json
+    final metadataFile = File('$imagePath.metadata.json');
+    final jsonString = metadataFile.readAsStringSync();
+
+    return ImageMetadata.fromJson(jsonString);
+  }
 }
