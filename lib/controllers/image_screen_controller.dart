9c8ca9a44ed1e00eafdd241c6897f0a08b415c28
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-16 23:56:40 +0800

    Update image_screen_controller.dart

--- lib/controllers/image_screen_controller.dart
+++ lib/controllers/image_screen_controller.dart
@@ -1,49 +1,99 @@
+import 'package:caonalyzer/gallery/gallery_writer.dart';
 import 'package:caonalyzer/gallery/metadata_reader.dart';
 import 'package:caonalyzer/gallery/models/image_metadata.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:caonalyzer/globals.dart' as globals;
-import 'package:get_storage/get_storage.dart';
+import 'package:caonalyzer/globals.dart';
 import 'package:image/image.dart' as image_lib;
+import 'package:path/path.dart' as path_lib;
 
 import '../gallery/metadata_writer.dart';
 
 class ImageScreenController extends GetxController {
-  final RxString imagePath = ''.obs;
-  final Rx<ImageMetadata?> imageMetadata = Rxn<ImageMetadata>();
+  final RxInt currentIndex = 0.obs;
+  final List<String> images;
+  late final List<ImageMetadata?> metadataImages;
+  late final PageController pageController;
+  final showingResultSheet = false.obs;
 
-  void loadImagePath(String imagePath_) {
-    imagePath.value = imagePath_;
-    imageMetadata.value = MetadataReader.read(imagePath_);
+  ImageScreenController(this.images, {int initialIndex = 0}) {
+    currentIndex.value = initialIndex;
+    pageController = PageController(initialPage: initialIndex);
   }
 
-  void rescanImage() async {
-    final objectDetector = globals.preferredMode.value.objectDetector;
+  @override
+  void onInit() {
+    super.onInit();
 
-    final tensorImage = objectDetector.preprocessImage(
-      (await image_lib.decodeJpgFile(imagePath.value))!,
-    );
-    final output = await objectDetector.runInference(tensorImage);
-
-    var metadata = ImageMetadata(
-      imagePath: imagePath.value,
-      objectDetectionMode: globals.preferredMode.value.toString(),
-      objectDetectionOutputs: output
-          .map((e) => ObjectDetectionOutput(
-                class_: e.label,
-                confidence: e.confidence,
-                boxes: [
-                  e.boundingBox.left,
-                  e.boundingBox.top,
-                  e.boundingBox.right,
-                  e.boundingBox.bottom,
-                ],
-              ))
-          .toList(),
-    );
-
-    MetadataWriter.create(
-      imagePath.value,
-      metadata,
-    );
+    metadataImages = images.map((e) => MetadataReader.read(e)).toList();
   }
+
+  void deleteCurrentImage() {
+    final imagePath = images[currentIndex.value];
+
+    if (MetadataReader.read(imagePath) != null) {
+      MetadataWriter.delete(imagePath);
+    }
+
+    GalleryWriter.removeImages([imagePath]);
+
+    images.removeAt(currentIndex.value);
+    metadataImages.removeAt(currentIndex.value);
+
+    if (images.isEmpty) {
+      final dirPath = path_lib.dirname(imagePath);
+      Globals.batches.removeWhere((element) => element.dirPath == dirPath);
+
+      Get.until((route) => route.isFirst);
+
+      return;
+    }
+
+    if (currentIndex.value == images.length) {
+      currentIndex.value--;
+    }
+
+    pageController.jumpToPage(currentIndex.value);
+  }
+
+  void scanCurrentImage() async {
+    final imagePath = images[currentIndex.value];
+
+    final metadata = MetadataReader.read(imagePath);
+
+    if (metadata == null) {
+      final objectDetector = Globals.preferredMode.value.objectDetector;
+      final tensorImage = objectDetector.preprocessImage(
+        (await image_lib.decodeJpgFile(imagePath))!,
+      );
+      final output = await objectDetector.runInference(tensorImage);
+
+      var metadata = ImageMetadata(
+        imagePath: imagePath,
+        objectDetectionMode: Globals.preferredMode.value.toString(),
+        objectDetectionOutputs: output
+            .map((e) => ObjectDetectionOutput(
+                  class_: e.label,
+                  confidence: e.confidence,
+                  boxes: [
+                    e.boundingBox.left,
+                    e.boundingBox.top,
+                    e.boundingBox.right,
+                    e.boundingBox.bottom,
+                  ],
+                ))
+            .toList(),
+      );
+
+      metadataImages[currentIndex.value] = metadata;
+    } else {
+      showingResultSheet.value = true;
+    }
+  }
+
+  void closeResultSheet() {
+    showingResultSheet.value = false;
+  }
+
+  ImageMetadata? get currentImageMetadata => metadataImages[currentIndex.value];
 }
