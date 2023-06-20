c009ff72f0d24cef9c664f4770f7cfca303310ad
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-20 19:44:42 +0800

    Update image_page.dart

--- lib/app/features/image/ui/image_page.dart
+++ lib/app/features/image/ui/image_page.dart
@@ -5,6 +5,10 @@ class ImagePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Image'),
+      ),
+    );
   }
 }
