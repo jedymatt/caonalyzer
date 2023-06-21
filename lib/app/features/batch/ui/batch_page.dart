0c6fcbe2b128175819458d123d397f015c8b1147
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-21 14:51:58 +0800

    Update batch_page.dart

--- lib/app/features/batch/ui/batch_page.dart
+++ lib/app/features/batch/ui/batch_page.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:caonalyzer/app/features/batch/bloc/batch_bloc.dart';
-import 'package:caonalyzer/app/features/batch/ui/widgets/batch_app_bar.dart';
 import 'package:caonalyzer/app/features/batch/ui/widgets/image_tile.dart';
 import 'package:caonalyzer/app/features/image/ui/image_page.dart';
 import 'package:flutter/material.dart';
@@ -13,29 +12,36 @@ class BatchPage extends StatefulWidget {
 
   final String batchPath;
 
+  static MaterialPageRoute route({required String batchPath}) {
+    return MaterialPageRoute(
+      builder: (_) => BatchPage(batchPath: batchPath),
+    );
+  }
+
   @override
   State<BatchPage> createState() => _BatchPageState();
 }
 
 class _BatchPageState extends State<BatchPage> {
   late final String title;
+  late final BatchBloc batchBloc;
 
   @override
   void initState() {
     super.initState();
 
     title = path_lib.basename(widget.batchPath);
+    batchBloc = BatchBloc()
+      ..add(BatchFetchImagesEvent(batchPath: widget.batchPath));
   }
 
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) =>
-          BatchBloc()..add(BatchFetchImagesEvent(batchPath: widget.batchPath)),
+      create: (context) => batchBloc,
       child: BlocBuilder<BatchBloc, BatchState>(
         buildWhen: (previous, current) => current is! BatchActionState,
         builder: (context, state) {
-          final batchBloc = BlocProvider.of<BatchBloc>(context);
           return WillPopScope(
             onWillPop: () async {
               if (state is BatchSelectionModeState) {
@@ -127,7 +133,8 @@ class _BatchPageState extends State<BatchPage> {
                           child: ImageTile(
                             image: FileImage(File(state.images[index])),
                             onTap: () {
-                              Navigator.of(context).push(MaterialPageRoute(builder: (context) => const ImagePage()));
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => const ImagePage()));
                             },
                             onLongPress: () {
                               batchBloc.add(BatchSelectMultipleImagesEvent(
@@ -152,4 +159,73 @@ class _BatchPageState extends State<BatchPage> {
       ),
     );
   }
+}
+
+class BatchAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const BatchAppBar({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    final batchBloc = context.read<BatchBloc>();
+
+    return BlocBuilder<BatchBloc, BatchState>(
+      builder: (context, state) {
+        if (state is BatchSelectionModeState) {
+          return AppBar(
+            title: Text('${state.selectedImages.length} selected'),
+            leading: IconButton(
+              onPressed: () {
+                batchBloc.add(BatchCancelSelectionModeEvent(
+                  images: state.images,
+                ));
+              },
+              icon: const Icon(Icons.close),
+            ),
+            actions: [
+              IconButton(
+                onPressed: () {
+                  batchBloc.add(BatchSelectMultipleImagesEvent(
+                    images: state.images,
+                    selectedImages: [...state.images],
+                  ));
+                },
+                icon: const Icon(Icons.select_all),
+              ),
+              IconButton(
+                // deselect
+                onPressed: () {
+                  batchBloc.add(BatchSelectMultipleImagesEvent(
+                    images: state.images,
+                    selectedImages: const [],
+                  ));
+                },
+                icon: const Icon(Icons.deselect),
+              ),
+            ],
+          );
+        }
+
+        if (state is BatchSuccessfulFetchImages) {
+          return AppBar(title: Text(title), actions: [
+            IconButton(
+              onPressed: () {
+                batchBloc.add(BatchSelectMultipleImagesEvent(
+                  images: state.images,
+                  selectedImages: const [],
+                ));
+              },
+              icon: const Icon(Icons.checklist),
+            )
+          ]);
+        }
+
+        return AppBar(title: Text(title));
+      },
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
