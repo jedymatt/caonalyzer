ecd6ffc960c7a00ee473accc0e0f4d5477d298d6
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-22 16:30:48 +0800

    Update gallery_bloc.dart

--- lib/app/features/gallery/bloc/gallery_bloc.dart
+++ lib/app/features/gallery/bloc/gallery_bloc.dart
@@ -14,31 +14,23 @@ class GalleryBloc extends Bloc<GalleryEvent, GalleryState> {
   GalleryBloc() : super(GalleryInitial()) {
     on<GalleryInitialEvent>((event, emit) async {
       emit(GalleryLoading());
-      final batchPaths = await GalleryReader.getBatchPaths();
-      var batches = batchPaths
-          .map((e) => Batch(
-                title: path_lib.basename(e),
-                directory: e,
-                thumbnail: Directory(e).listSync().first.path,
-              ))
-          .toList();
-
-      emit(GalleryLoaded(batches: batches));
+      emit(GalleryLoaded(batches: await _getBatches()));
     });
 
-    on<GalleryFetchImagesEvent>((event, emit) async {
-      emit(GalleryLoading());
-
-      final batchPaths = await GalleryReader.getBatchPaths();
-      var batches = batchPaths
-          .map((e) => Batch(
-                title: path_lib.basename(e),
-                directory: e,
-                thumbnail: Directory(e).listSync().first.path,
-              ))
-          .toList();
-
-      emit(GalleryLoaded(batches: batches));
+    on<GalleryRefreshImagesEvent>((event, emit) async {
+      emit(GalleryRefreshing(batches: event.placeholderBatches));
+      emit(GalleryLoaded(batches: await _getBatches()));
     });
+  }
+
+  Future<List<Batch>> _getBatches() async {
+    final batchPaths = await GalleryReader.getBatchPaths();
+    return batchPaths
+        .map((e) => Batch(
+              title: path_lib.basename(e),
+              directory: e,
+              thumbnail: Directory(e).listSync().first.path,
+            ))
+        .toList();
   }
 }
