4b3eed73157dd09ab47265233afa5d16068c8a3d
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-23 13:25:38 +0800

    Update home_page.dart

--- lib/app/features/home/ui/home_page.dart
+++ lib/app/features/home/ui/home_page.dart
@@ -1,7 +1,9 @@
 import 'package:caonalyzer/app/features/camera/ui/camera_page.dart';
 import 'package:caonalyzer/app/features/gallery/ui/gallery_fragment.dart';
+import 'package:caonalyzer/app/features/home/bloc/home_bloc.dart';
 import 'package:caonalyzer/app/features/settings/ui/settings_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -11,7 +13,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int selectedTab = 0;
+  late final HomeBloc homeBloc;
+  @override
+  void initState() {
+    super.initState();
+
+    homeBloc = BlocProvider.of<HomeBloc>(context);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,32 +38,41 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: Builder(builder: (context) {
-        switch (selectedTab) {
-          case 0:
-            return const Center(child: Text('Home'));
-          case 1:
-            return const GalleryFragment();
-        }
+      body: BlocBuilder<HomeBloc, HomeState>(
+        builder: (context, state) {
+          final initialState = state as HomeInitial;
 
-        return const Center(child: Text('404 Not Found'));
-      }),
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: selectedTab,
-        destinations: const [
-          NavigationDestination(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          NavigationDestination(
-            icon: Icon(Icons.photo_library),
-            label: 'Gallery',
-          ),
-        ],
-        onDestinationSelected: (value) {
-          setState(() {
-            selectedTab = value;
-          });
+          switch (initialState.tab) {
+            case HomeTab.home:
+              return const Center(child: Text('Home'));
+            case HomeTab.gallery:
+              return const GalleryFragment();
+            default:
+              return const SizedBox.shrink();
+          }
+        },
+      ),
+      bottomNavigationBar: BlocBuilder<HomeBloc, HomeState>(
+        builder: (context, state) {
+          final initialState = state as HomeInitial;
+          return NavigationBar(
+            selectedIndex: initialState.tab.index,
+            destinations: const [
+              NavigationDestination(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              NavigationDestination(
+                icon: Icon(Icons.photo_library),
+                label: 'Gallery',
+              ),
+            ],
+            onDestinationSelected: (index) {
+              homeBloc.add(HomeTabChangedEvent(
+                tab: HomeTab.values[index],
+              ));
+            },
+          );
         },
       ),
       floatingActionButton: FloatingActionButton.extended(
