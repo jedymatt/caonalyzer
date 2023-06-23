872d6395e06e323945c169969bdba61f79016e63
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-23 13:26:02 +0800

    Update batch_bloc.dart

--- lib/app/features/batch/bloc/batch_bloc.dart
+++ lib/app/features/batch/bloc/batch_bloc.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:caonalyzer/gallery/gallery_reader.dart';
+import 'package:caonalyzer/gallery/gallery_writer.dart';
 import 'package:meta/meta.dart';
+import 'package:path/path.dart' as path_lib;
 
 part 'batch_event.dart';
 
@@ -20,13 +22,37 @@ class BatchBloc extends Bloc<BatchEvent, BatchState> {
 
     on<BatchSelectMultipleImagesEvent>((event, emit) {
       emit(BatchSelectionModeState(
-        images: event.images,
-        selectedImages: event.selectedImages.toSet().toList()
-      ));
+          images: event.images,
+          selectedImages: event.selectedImages.toSet().toList()));
     });
 
     on<BatchCancelSelectionModeEvent>((event, emit) {
       emit(BatchSuccessfulFetchImages(images: event.images));
+    });
+
+    on<BatchDeleteImagesEvent>((event, emit) async {
+      emit(BatchDeletingImagesState());
+      GalleryWriter.removeImages(event.images);
+      final batchPath = path_lib.dirname(event.images.first);
+
+      final remainingImages = (await GalleryReader.getImages(batchPath))
+          .map((e) => e.path)
+          .toList();
+
+      if (remainingImages.isEmpty) {
+        GalleryWriter.deleteDirectory(batchPath);
+        emit(BatchNavigateToParentPageActionState());
+        return;
+      }
+
+      emit(BatchSuccessfulFetchImages(
+        images: remainingImages,
+      ));
+
+      if (remainingImages.isEmpty) {
+        GalleryWriter.deleteDirectory(batchPath);
+        emit(BatchNavigateToParentPageActionState());
+      }
     });
   }
 }
