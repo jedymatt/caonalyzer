9640bdb58cd0c4072302cc0258b654f851eac923
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-24 21:55:22 +0800

    Update gallery_state.dart

--- lib/app/features/gallery/bloc/gallery_state.dart
+++ lib/app/features/gallery/bloc/gallery_state.dart
@@ -7,21 +7,14 @@ abstract class GalleryActionState extends GalleryState {}
 
 class GalleryInitial extends GalleryState {}
 
-class GalleryLoading extends GalleryState {}
+class GalleryInProgress extends GalleryState {}
 
 class GallerySuccess extends GalleryState {
   final List<Batch> batches;
-  final bool refreshing;
 
-  GallerySuccess({required this.batches, this.refreshing = false});
-
-  GallerySuccess copyWith({
-    List<Batch>? batches,
-    bool? refreshing,
-  }) {
-    return GallerySuccess(
-      batches: batches ?? this.batches,
-      refreshing: refreshing ?? this.refreshing,
-    );
-  }
+  GallerySuccess({required this.batches});
 }
+
+class GalleryRefreshInProgress extends GalleryState {}
+
+class GalleryRefreshSuccess extends GalleryState {}
