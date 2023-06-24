0bdf344ca7cecdf5ae89b3e7d58c976b6c92a1e0
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-24 20:13:13 +0800

    Update camera_bloc.dart

--- lib/app/features/camera/bloc/camera_bloc.dart
+++ lib/app/features/camera/bloc/camera_bloc.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:camera/camera.dart';
-import 'package:caonalyzer/gallery/gallery_writer.dart';
 import 'package:caonalyzer/globals.dart';
 import 'package:meta/meta.dart';
 
@@ -11,7 +10,8 @@ part 'camera_state.dart';
 class CameraBloc extends Bloc<CameraEvent, CameraState> {
   late CameraController _cameraController;
 
-  CameraBloc() : super(CameraInitial()) {
+  CameraBloc({required CameraCaptureMode mode})
+      : super(CameraInitial(mode: mode)) {
     on<CameraStarted>(_onStarted);
     on<CameraStopped>(_onStopped);
     on<CameraCaptured>(_onCaptured);
@@ -21,17 +21,18 @@ class CameraBloc extends Bloc<CameraEvent, CameraState> {
 
   FutureOr<void> _onStarted(
       CameraStarted event, Emitter<CameraState> emit) async {
+    if (state is! CameraInitial) return;
+    final state_ = state as CameraInitial;
     try {
       _cameraController = CameraController(
         Globals.cameras.first,
         ResolutionPreset.low,
         enableAudio: false,
       );
+
       await _cameraController.initialize();
-      emit(CameraReady(
-        mode: event.mode,
-        images: const [],
-      ));
+
+      emit(CameraReady(mode: state_.mode));
     } on CameraException catch (e) {
       _cameraController.dispose();
       emit(CameraFailure(message: e.description!));
@@ -42,7 +43,10 @@ class CameraBloc extends Bloc<CameraEvent, CameraState> {
 
   FutureOr<void> _onStopped(CameraStopped event, Emitter<CameraState> emit) {
     _cameraController.dispose();
-    emit(CameraInitial());
+    if (state is! CameraReady) return null;
+
+    final state_ = state as CameraReady;
+    emit(CameraInitial(mode: state_.mode));
   }
 
   FutureOr<void> _onCaptured(
@@ -54,20 +58,10 @@ class CameraBloc extends Bloc<CameraEvent, CameraState> {
     try {
       emit(CameraCaptureInProgress());
 
-      if (state_.mode == CameraCaptureMode.batch) {
-        final image = await _cameraController.takePicture();
+      final image = await _cameraController.takePicture();
 
-        emit(state_.copyWith(
-          images: List.from(state_.images)..add(image.path),
-          batchPath: state_.images.isEmpty
-              ? await GalleryWriter.generateBatchPath(DateTime.now())
-              : state_.batchPath,
-        ));
-      } else {
-        final image = await _cameraController.takePicture();
-
-        emit(CameraCaptureSuccess(path: image.path));
-      }
+      emit(CameraCaptureSuccess(path: image.path, mode: state_.mode));
+      emit(CameraReady(mode: state_.mode));
     } on CameraException catch (e) {
       emit(CameraCaptureFailure(message: e.description!));
     }
