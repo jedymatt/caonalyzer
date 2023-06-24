cac61bc8ea55043623bc5f549783628a5657b118
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-24 16:54:32 +0800

    Create scaled_camera_preview.dart

---
+++ lib/app/global_widgets/scaled_camera_preview.dart
@@ -0,0 +1,32 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class ScaledCameraPreview extends StatelessWidget {
+  final CameraController controller;
+
+  const ScaledCameraPreview(this.controller, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+
+    // fetch screen size
+    final size = MediaQuery.of(context).size;
+
+    // calculate scale depending on screen and camera ratios
+    // this is actually size.aspectRatio / (1 / camera.aspectRatio)
+    // because camera preview size is received as landscape
+    // but we're calculating for portrait orientation
+    var scale = size.aspectRatio * controller.value.aspectRatio;
+
+    // to prevent scaling down, invert the value
+    if (scale < 1) scale = 1 / scale;
+
+    return Transform.scale(
+      scale: scale,
+      child: Center(child: CameraPreview(controller)),
+    );
+  }
+}
