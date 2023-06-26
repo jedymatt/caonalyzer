5c05a0eea9810da3957baccf9ec1cc7124cde6c2
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-26 21:46:17 +0800

    Declare constant variable settings box name

--- lib/globals.dart
+++ lib/globals.dart
@@ -5,6 +5,7 @@ import 'package:get_storage/get_storage.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
 const String kAppName = 'Cao-nalyzer';
+const String kSettingsBoxName = 'settings';
 
 final box = GetStorage();
 
@@ -16,6 +17,8 @@ Future<void> initGlobals() async {}
 abstract final class Globals {
   static final preferredMode = PreferredMode.offline.obs;
   static final cameras = <CameraDescription>[].obs;
+  static final host = '192.168.1.8'.obs;
+  static final port = '8000'.obs;
 
   static Future<void> init() async {
     cameras.value = await availableCameras();
