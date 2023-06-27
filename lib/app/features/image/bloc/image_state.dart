07dffebfe5838c36749afc3312b3dbb3f01055fa
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-27 22:40:27 +0800

    Update image_state.dart

--- lib/app/features/image/bloc/image_state.dart
+++ lib/app/features/image/bloc/image_state.dart
@@ -6,15 +6,26 @@ abstract class ImageState {}
 class ImageInitial extends ImageState {
   final List<String> images;
   final int index;
-  ImageInitial({required this.images, this.index = 0});
+  final bool showDetection;
+  final bool detectionInProgress;
+  ImageInitial({
+    required this.images,
+    this.index = 0,
+    this.showDetection = false,
+    this.detectionInProgress = false,
+  });
 
   ImageInitial copyWith({
     List<String>? images,
     int? index,
+    bool? showDetection,
+    bool? detectionInProgress,
   }) {
     return ImageInitial(
       images: images ?? this.images,
       index: index ?? this.index,
+      showDetection: showDetection ?? this.showDetection,
+      detectionInProgress: detectionInProgress ?? this.detectionInProgress,
     );
   }
 }
