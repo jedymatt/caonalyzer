4656444c61a1a4c9289604ebfd2fa9d2a9282083
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-27 22:39:44 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:caonalyzer/app/features/gallery/bloc/gallery_bloc.dart';
 import 'package:caonalyzer/app/features/home/bloc/home_bloc.dart';
 import 'package:caonalyzer/app/features/home/ui/home_page.dart';
 import 'package:caonalyzer/globals.dart';
+import 'package:caonalyzer/hive_adapters.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/hive_flutter.dart';
@@ -9,6 +10,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
+  Hive.registerAdapter(PreferredModeAdapter());
   await Globals.init();
 
   Bloc.observer = AppBlocObserver();
