fd9ef18db315fbc89997363181926216e3dd3335
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-06-28 17:02:59 +0800

    Update image.dart

--- lib/app/features/image/models/image.dart
+++ lib/app/features/image/models/image.dart
@@ -1,3 +1,5 @@
+import 'package:caonalyzer/app/data/models/models.dart';
+
 class Image {
   final String path;
   final List<DetectedObject>? detectedObjects;
@@ -18,24 +20,3 @@ class Image {
   }
 }
 
-class DetectedObject {
-  final String label;
-  final double confidence;
-  final List<double> boundingBox;
-  DetectedObject({
-    required this.label,
-    required this.confidence,
-    required this.boundingBox,
-  });
-
-  List<double> absoluteBoundingBox(double height, double width) {
-    return [
-      boundingBox[0] * width,
-      boundingBox[1] * height,
-      boundingBox[2] * width,
-      boundingBox[3] * height,
-    ];
-  }
-
-  String get displayLabel => '$label ${(confidence * 100).toStringAsFixed(2)}%';
-}
