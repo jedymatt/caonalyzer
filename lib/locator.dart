bd2c060819fdd8644dc1c5f96ca943822de82347
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-07-07 17:31:15 +0800

    Update locator.dart

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,4 +1,5 @@
 import 'package:caonalyzer/app/data/services/detected_object_service.dart';
+import 'package:caonalyzer/app/data/services/realtime_pytorch_object_detector.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive/hive.dart';
 
@@ -8,6 +9,8 @@ void setupLocator() async {
   await Hive.openBox('detected_objects');
 
   locator.registerLazySingleton<DetectedObjectService>(
-    () => DetectedObjectService(Hive.box('detected_objects')),
-  );
+      () => DetectedObjectService(Hive.box('detected_objects')));
+
+  locator.registerLazySingleton<RealtimePytorchObjectDetector>(
+      () => RealtimePytorchObjectDetector());
 }
