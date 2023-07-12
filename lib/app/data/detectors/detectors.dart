e916144b07cea9b665be9428aacfd4445cd9bcd0
Jedy Matt Tabasco <jedymatt@gmail.com>
2023-07-12 20:14:43 +0800

    Remove unused classes

--- lib/app/data/detectors/detectors.dart
+++ lib/app/data/detectors/detectors.dart
@@ -1,3 +1,2 @@
 export 'pytorch_object_detector.dart';
-export 'realtime_pytorch_object_detector.dart';
 export 'tf_serving_object_detector.dart';
--- lib/object_detector/object_detector.dart
+++ lib/object_detector/object_detector.dart
@@ -1,4 +1,3 @@
 export 'src/object_detector.dart';
 export 'src/object_detection_output.dart';
 export 'src/bounding_box.dart';
-export 'src/realtime_object_detector.dart';
