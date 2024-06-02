bbd86ccf9c43a1f4ba0adc19772646993f5172a8
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2024-06-02 21:59:25 +0800

    revert the removed singleton registration

--- lib/locator.dart
+++ lib/locator.dart
@@ -1,4 +1,5 @@
 import 'package:caonalyzer/app/data/services/detected_object_service.dart';
+import 'package:caonalyzer/app/data/utils/object_detector_settings.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive/hive.dart';
 
@@ -8,6 +9,12 @@ void setupLocator() async {
   locator.registerSingletonAsync<DetectedObjectService>(
     () async => DetectedObjectService(await Hive.openBox(
       'detected_objects',
+    )),
+  );
+
+  locator.registerSingletonAsync<ObjectDetectorSettings>(
+    () async => ObjectDetectorSettings(await Hive.openBox(
+      'object_detector_settings',
     )),
   );
 }
